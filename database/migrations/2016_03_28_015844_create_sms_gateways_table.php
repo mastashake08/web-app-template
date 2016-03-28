f213542abd7d81f7395b8815135e6d19fa551759
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-28 02:03:15 +0000

    Created SmsGateway model and migration

---
+++ database/migrations/2016_03_28_015844_create_sms_gateways_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSmsGatewaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms_gateways', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sms_gateways');
+    }
+}
