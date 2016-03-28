35b1828ed727153deb8c09744edb6283a7988752
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-28 01:55:58 +0000

    Added UsersTableSeeder Logic

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -12,5 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        User::Create([
+          'email' => 'admin@domain.com',
+          'password' => bcrypt('password')
+          ]);
     }
 }
